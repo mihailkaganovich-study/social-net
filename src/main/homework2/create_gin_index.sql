1847debc45729f3d3ea6a63151db715383b41823
Mihail Kaganovich <mihail_kaganovich@center.rt.ru>
2026-03-22 08:49:00 +0300

    fix create index

--- src/main/homework2/create_gin_index.sql
+++ src/main/homework2/create_gin_index.sql
@@ -1,1 +1,8 @@
 -- Включение расширения
+CREATE EXTENSION pg_trgm;
+CREATE INDEX idx_gin_users 
+ON users USING GIN (first_name gin_trgm_ops, second_name gin_trgm_ops);
+
+--такой индекс был выбран исходя из запроса где поиск идет по вхождению строки внутри
+-- т.е. LIKE '%abc%'
+--B-tree в такого рода запросах не работает
